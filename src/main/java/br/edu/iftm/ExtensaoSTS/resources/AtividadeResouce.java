f43226db2455dc382ffe4ae5730f6707724a243d
ronneypetras <ronneypetras@gmail.com>
2018-09-29 11:30:57 -0300

    versao 2 sistema

--- src/main/java/br/edu/iftm/ExtensaoSTS/resources/AtividadeResouce.java
+++ src/main/java/br/edu/iftm/ExtensaoSTS/resources/AtividadeResouce.java
@@ -1,17 +1,33 @@
 package br.edu.iftm.ExtensaoSTS.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.iftm.ExtensaoSTS.domain.Atividade;
 
 @RestController
 @RequestMapping (value="/atividades")
 public class AtividadeResouce {
 	
 	@RequestMapping (method=RequestMethod.GET)
-	public String listar () {
-		return "Rest ok";
+	public List<Atividade> listar(){
+		Atividade a1 = new Atividade(1,"visita tecnica dgd");
+		Atividade a2 = new Atividade(2,"simpós");
+		return Arrays.asList(a1,a2);
+		
+		
+		
 	}
+	
+	
+	//@RequestMapping (method=RequestMethod.GET)
+	//public String listar () {
+	//	return "Rest ok";
+	//}
 	
 
 }
